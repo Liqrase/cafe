891bf931c51a161e6b0e7e98291b86271c88eef9
Liqrase <qarnqitana9w4@gmail.com>
2025-02-21 12:20:39 +0900

    4

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
     navbar.classList.toggle('open');
     overlay.classList.toggle('show');
 });
+
+// 背景がクリックされたときにメニューを閉じる
+document.getElementById('menu-overlay').addEventListener('click', function() {
+    const navbar = document.getElementById('navbar');
+    const overlay = document.getElementById('menu-overlay');
+
+    // メ
