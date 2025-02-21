b30a0217676e3b6ea35d74591fc99962372ddb3d
Liqrase <qarnqitana9w4@gmail.com>
2025-02-21 12:21:43 +0900

    4.1

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
     const navbar = document.getElementById('navbar');
     const overlay = document.getElementById('menu-overlay');
 
-    // メニューを表示・非表示
     navbar.classList.toggle('open');
     overlay.classList.toggle('show');
 });
 
-// 背景がクリックされたときにメニューを閉じる
+// 背景をクリックしたときにメニューを閉じる
 document.getElementById('menu-overlay').addEventListener('click', function() {
     const navbar = document.getElementById('navbar');
     const overlay = document.getElementById('menu-overlay');
 
-    // メ
+    navbar.classList.remove('open');
+    overlay.classList.remove('show');
+});
