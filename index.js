938a9661312f38505f976258226e5f197bbcd85f
Liqrase <qarnqitana9w4@gmail.com>
2025-02-21 12:55:46 +0900

    7

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-// ハンバーガーメニューをクリックして表示/非表示
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const navbar = document.querySelector('nav');
-    const overlay = document.getElementById('menu-overlay');
-    
-    navbar.classList.toggle('open');
-    overlay.classList.toggle('show');
-});
+// ハンバーガーメニューの表示/非表示切り替え
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-// 背景部分をクリックしたときにメニューを閉じる
-document.getElementById('menu-overlay').addEventListener('click', function() {
-    const navbar = document.querySelector('nav');
-    const overlay = document.getElementById('menu-overlay');
-    
-    navbar.classList.remove('open');
-    overlay.classList.remove('show');
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
 });
