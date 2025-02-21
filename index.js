f13d80f5ca2ba2cb885f08abf3a290cc27b9f648
Liqrase <qarnqitana9w4@gmail.com>
2025-02-21 11:24:17 +0900

    3

--- index.js
+++ index.js
@@ -3,5 +3,9 @@
 // ハンバーガーメニューをクリックしたときにナビゲーションメニューを表示/非表示
 document.getElementById('menu-toggle').addEventListener('click', function() {
     const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('open');  // 'open'クラスをトグル
+    const overlay = document.getElementById('menu-overlay');
+
+    // メニューを表示・非表示
+    navbar.classList.toggle('open');
+    overlay.classList.toggle('show');
 });
